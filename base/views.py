3ce169fdd546e209e32a6007de60e9cb02969482
Kevin Venegas <kevinv234@gmail.com>
2022-04-12 22:09:10 -0700

    StudyExchange 1.0
    - Adjusting views
    - Creating data structures that will be used by views.

--- base/views.py
+++ base/views.py
@@ -1,10 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+rooms = [
+    {'id': 1, 'name': 'Python project'},
+    {'id': 2, 'name': 'FrontEnd'},
+    {'id': 3, 'name': 'Backend'},
+]
+
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+
+    context = {'room': rooms}
+    return render(request, 'base/room.html')
